f74e0679be87f796c84e34fbb87ff6c920bf07bd
Michael Lam <m.lam08@outlook.com>
2023-09-23 22:45:11 -0400

    implement cli markdown renderer

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/charmbracelet/glamour"
+	"golang.org/x/term"
+)
 
 func main() {
-	fmt.Println("clinote")
+	
+	// Test Markdown
+	test := `# Hello World
+
+This is a simple example of Markdown rendering with Glamour!
+Check out the [other examples](https://github.com/charmbracelet/glamour/tree/master/examples) too.
+
+Bye!
+`
+
+	terminalWidth, _, err := term.GetSize(0)
+
+	renderer, err := glamour.NewTermRenderer(
+			glamour.WithAutoStyle(),
+			glamour.WithWordWrap(terminalWidth-5))
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	out, _ := renderer.Render(test)
+
+	fmt.Print(out)
 }
